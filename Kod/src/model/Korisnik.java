0db09f56a4d977fdbc56d26a2521b7b29326c00a
Unknown <nikolanemzi@gmail.com>
2018-07-11 17:49:30 +0200

    Korisnik

--- Kod/src/model/Korisnik.java
+++ Kod/src/model/Korisnik.java
@@ -1,4 +1,62 @@
 package model;
 
 public class Korisnik {
+
+    private TipKorisnika tip;
+    private String korisnickoIme;
+    private String lozinka;
+    private String ime;
+    private String prezime;
+
+    public Korisnik() {
+
+    }
+
+    public Korisnik(TipKorisnika tip, String korisnickoIme, String lozinka, String ime, String prezime) {
+        this.tip = tip;
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+
+    public TipKorisnika getTip() {
+        return tip;
+    }
+
+    public void setTip(TipKorisnika tip) {
+        this.tip = tip;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
 }
---
+++ Kod/src/model/TipKorisnika.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum TipKorisnika {
+    RADNIK_U_STANICI, SEF_STANICE, RADNIK_U_CENTRALI
+}
