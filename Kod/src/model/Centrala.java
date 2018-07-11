3fac6bdc1e2a97902de1344f527374028a93fa7c
Unknown <nikolanemzi@gmail.com>
2018-07-11 17:59:20 +0200

    Kod

--- Kod/src/model/Centrala.java
+++ Kod/src/model/Centrala.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Centrala {
+
+    private ArrayList<Korisnik> korisnici;
+    private ArrayList<Deonica> deonice;
+    private ArrayList<RadnaStanica> radneStanice;
+    private String naziv;
+
+    public Centrala() {
+        this.korisnici = new ArrayList<Korisnik>();
+        this.deonice = new ArrayList<Deonica>();
+        this.radneStanice = new ArrayList<RadnaStanica>();
+    }
+
+    void dodajRadnuStanicu(RadnaStanica radnaStanica) {
+        this.radneStanice.add(radnaStanica);
+    }
+
+    void izbrisiRadnuStanicu(RadnaStanica radnaStanica) {
+        for (RadnaStanica i : this.radneStanice) {
+            
+        }
+    }
+
 }
