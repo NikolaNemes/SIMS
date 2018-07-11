99d1a871cce5374640bcde57fd49f40baf26ac40
bandjeo <branislav.andjelic@gmail.com>
2018-07-11 18:57:20 +0200

    Napravljen login prozor, bez konkretne akcije

--- Kod/src/Main.java
+++ Kod/src/Main.java
@@ -3,5 +3,6 @@ import viewer.LoginWindow;
 public class Main {
     public static void main(String[] args){
         LoginWindow lw = new LoginWindow();
+
     }
 }
--- Kod/src/viewer/LoginWindow.java
+++ Kod/src/viewer/LoginWindow.java
@@ -1,12 +1,35 @@
 package viewer;
 
+import net.miginfocom.swing.MigLayout;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginWindow extends JFrame{
 
     public LoginWindow() throws HeadlessException {
-        setLayout(null);
+        setSize(450, 200);
+        setLayout(new MigLayout("wrap 5"));
+        JLabel welcomeLabel = new JLabel("WELCOME");
+        JLabel usernameLabel = new JLabel("Username: ");
+        JTextField usernameField = new JTextField(15);
+        JLabel passwordLabel = new JLabel("Password: ");
+        JPasswordField passwordField = new JPasswordField(15);
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                
+            }
+        });
+
+        add(welcomeLabel, "span 2, wrap");
+        add(usernameLabel);
+        add(usernameField, "wrap");
+        add(passwordLabel);
+        add(passwordField, "wrap");
+        add(loginButton);
         setVisible(true);
     }
 }
