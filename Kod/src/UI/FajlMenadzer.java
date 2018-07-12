0e208a46b40a9e420ce3886d7455532bd42470b8
Unknown <nikolanemzi@gmail.com>
2018-07-12 22:16:10 +0200

    Xstream dodat

--- Kod/src/UI/FajlMenadzer.java
+++ Kod/src/UI/FajlMenadzer.java
@@ -1,46 +1,33 @@
 package UI;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import model.Centrala;
-import model.Korisnik;
-import model.TipKorisnika;
 
 import java.io.*;
-import java.util.ArrayList;
+
 
 
 public class FajlMenadzer {
 
-    public static void ucitajKorisnike(String korisnickiFajl, Centrala centrala) {
-        BufferedReader bf = null;
-        String linija = null;
-        try {
-            bf = new BufferedReader(new FileReader(korisnickiFajl));
-            while ((linija = bf.readLine()) != null) {
-                String[] delovi = linija.split("\\|");
-                TipKorisnika tipKorisnika = TipKorisnika.odrediTip(delovi[0]);
-                centrala.getKorisnici().add(new Korisnik(tipKorisnika, delovi[1], delovi[2], delovi[3], delovi[4], delovi[5]));
-            }
-            bf.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static Centrala ucitajKorisnike(String korisnickiFajl, Centrala centrala) {
+        XStream xstream = new XStream(new DomDriver());
+        Centrala retVal = (Centrala)xstream.fromXML(new File(korisnickiFajl));
+        return retVal;
     }
 
     public static void snimiKorisnike(String korisnickiFajl, Centrala centrala) {
+        XStream xstream = new XStream(new DomDriver());
+        String xml = xstream.toXML(centrala);
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(new FileWriter(korisnickiFajl));
-            for (Korisnik i: centrala.getKorisnici()) {
-                pw.write(i.toString() + "\n");
-            }
+            pw.print(xml);
             pw.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
-
 }
 
 
