c299cdf42cd2d18b09c9e8931f75fd3af4e40db4
Unknown <nikolanemzi@gmail.com>
2018-07-13 14:49:52 +0200

    Test primeri doradjeni

--- Kod/src/Application.java
+++ Kod/src/Application.java
@@ -1,13 +1,12 @@
 import UI.FajlMenadzer;
 import controller.LoginActions;
-import model.Centrala;
-import model.Deonica;
-import model.ProlazakVozila;
-import model.RadnaStanica;
+import model.*;
 import viewer.CentralWorkerWindow;
 import viewer.LoginWindow;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Application {
     public static void main(String[] args){
@@ -35,21 +34,41 @@ public class Application {
 
         ArrayList<Deonica> deonice = centrala.getDeonice();
 
+        HashMap<KategorijaVozila, Integer> ceneMapa = new HashMap<KategorijaVozila, Integer>();
+        ceneMapa.put(KategorijaVozila.A, 100);
+        ceneMapa.put(KategorijaVozila.B, 100);
+        ceneMapa.put(KategorijaVozila.C, 100);
+        ceneMapa.put(KategorijaVozila.D, 100);
+        NaplatneCene cene = new NaplatneCene(new Date(), null, ceneMapa, "2");
+
+
         Deonica vrbasNoviSad = new Deonica(vrbas, noviSad, "1");
+        vrbasNoviSad.dodajNaplatneCene(cene);
         Deonica vrbasNis = new Deonica(vrbas, nis, "2");
+        vrbasNis.dodajNaplatneCene(cene);
         Deonica vrbasValjevo = new Deonica(vrbas, valjevo, "3");
+        vrbasValjevo.dodajNaplatneCene(cene);
 
         Deonica noviSadVrbas = new Deonica(noviSad, vrbas, "4");
+        noviSadVrbas.dodajNaplatneCene(cene);
         Deonica noviSadNis = new Deonica(noviSad, nis, "5");
+        noviSadNis.dodajNaplatneCene(cene);
         Deonica noviSadValjevo = new Deonica(noviSad, valjevo, "6");
+        noviSadValjevo.dodajNaplatneCene(cene);
 
         Deonica nisVrbas = new Deonica(nis, vrbas, "7");
+        nisVrbas.dodajNaplatneCene(cene);
         Deonica nisNoviSad = new Deonica(nis, noviSad, "8");
+        nisNoviSad.dodajNaplatneCene(cene);
         Deonica nisValjevo = new Deonica(nis, valjevo, "9");
+        nisValjevo.dodajNaplatneCene(cene);
 
         Deonica valjevoVrbas = new Deonica(valjevo, vrbas, "10");
+        valjevoVrbas.dodajNaplatneCene(cene);
         Deonica valjevoNoviSad = new Deonica(valjevo, noviSad, "11");
+        valjevoNoviSad.dodajNaplatneCene(cene);
         Deonica valjevoNis = new Deonica(valjevo, nis, "12");
+        valjevoNis.dodajNaplatneCene(cene);
 
         deonice.add(vrbasNoviSad);
         deonice.add(vrbasNis);
